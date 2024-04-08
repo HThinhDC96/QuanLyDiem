8c74076528de17ae9885580a0c50b5c1232bfcf8
HThinhDC96 <thinhdc2196n527@vlvh.ctu.edu.vn>
2024-04-08 17:55:59 +0700

    Them Css form login

--- QuanLyDiem/resources/views/auth/login.blade.php
+++ QuanLyDiem/resources/views/auth/login.blade.php
@@ -1,18 +1,32 @@
-@extends('layouts.admin.layout')
+@extends('layouts.login.layout')
 
+@section('styles')
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
 @section('content')
-    <form action="{{ route('xacthuc-login') }}" method="post">
-        @csrf
-        <div>
-            <label>Tên đăng nhập* </label>
-            <input type="text" name="username" placeholder="Nhập username" />
+    <div class="d-flex full">
+        <div class="form-login">
+            <div class="login text-center p-2">
+                <form action="{{ route('xacthuc-login') }}" method="post">
+                    @csrf
+                    <div class="login-title">
+                        <h3>Đăng nhập</h3>
+                    </div>
+
+                    <div class="mb-3">
+                        <input type="text" class="form-control" id="username" name="username"
+                            placeholder="Nhập username" />
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="password" name="password"
+                            placeholder="Nhập mật khẩu" />
+                    </div>
+                    <div>
+                        <input class="btn btn-success" type="submit" value="Đăng nhập" />
+                    </div>
+                </form>
+            </div>
         </div>
-        <div>
-            <label>Mật khẩu* </label>
-            <input type="password" name="password" placeholder="Nhập mật khẩu" />
-        </div>
-        <div>
-            <input type="submit" value="Đăng nhập" />
-        </div>
-    </form>
+    </div>
 @endsection
