1801b0381e273a40705e1dc6d2850dc47bf7f38c
hp <hp@LAPTOP-DP4TP1GE>
2024-04-12 12:30:05 +0700

    translate datatable to vietnamese

--- QuanLyDiem/public/js/crud/canbo_datatables.js
+++ QuanLyDiem/public/js/crud/canbo_datatables.js
@@ -1,3 +1,27 @@
 $('#danhSachCanBo').DataTable({
     reponsive : "true",
+    "language": {
+        // "sProcessing":    "Procesando...",
+        "sLengthMenu":    "Hiển thị  _MENU_ mục trên mỗi trang",
+        "sZeroRecords":   "Không tìm thấy kết quả",
+        // "sEmptyTable":    "Ningún dato disponible en esta tabla",
+        "sInfo":          "Hiển thị từ _START_ đến _END_ trong tổng số _TOTAL_ mục",
+        "sInfoEmpty":     "Hiển thị từ 0 đến 0 trong tổng số 0 mục",
+        "sInfoFiltered":  "(được lọc từ tổng số _MAX_ mục)",
+        "sInfoPostFix":   "",
+        "sSearch":        "Tìm kiếm:",
+        "sUrl":           "",
+        "sInfoThousands":  ",",
+        // "sLoadingRecords": "Cargando...",
+        // "oPaginate": {
+        //     "sFirst":    "Đầu trang",
+        //     "sLast":    "Trước đó",
+        //     "sNext":    "Tiếp theo",
+        //     "sPrevious": "Cuối trang"
+        // },
+        // "oAria": {
+        //     "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        //     "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        // }
+    }
 });
