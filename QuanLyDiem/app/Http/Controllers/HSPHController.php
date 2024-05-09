cae178fbd82d3b9fef5b5696d3c1ef558c38aa32
HThinhDC96 <thinhdc2196n527@vlvh.ctu.edu.vn>
2024-05-09 09:22:54 +0700

    add: phuhuynh

--- QuanLyDiem/app/Http/Controllers/HSPHController.php
+++ QuanLyDiem/app/Http/Controllers/HSPHController.php
@@ -36,9 +36,9 @@ class HSPHController extends Controller
     {
         try {
             $hocsinh = new HocSinh();
-            if (HocSinh::where('mahocsinh', $request->mahocsinh)->first()) {
-                return redirect()->back()->withInput();
-            }
+            // if (HocSinh::where('mahocsinh', $request->mahocsinh)->first()) {
+            //     return redirect()->back()->withInput();
+            // }
 
             $hocsinh->fill($request->toArray());
 
@@ -120,6 +120,82 @@ class HSPHController extends Controller
         //  }
         confirmDelete("", "");
         // dd(session()->all());
-        return view('pages.hocsinh.giaovien.indexhocsinh', compact('page_title', 'data'));
+        return view('pages.hocsinh_phuhuynh.phuhuynh.index', compact('page_title', 'data'));
+    }
+
+    public function create_PH()
+    {
+        $page_title = "Thêm mới - Phụ huynh";
+        return view('pages.hocsinh_phuhuynh.phuhuynh.create', compact('page_title'));
+    }
+    public function store_PH(Request $request)
+    {
+        try {
+            $phuhuynh = new PhuHuynh();
+            $phuhuynh->fill($request->toArray());
+
+            // Khởi tạo mã cán bộ mới
+            $ph_lastest = PhuHuynh::latest('maphuhuynh')->select('maphuhuynh')->first();
+            if ($ph_lastest == null) {
+                $mph_lastest = "PH00001";
+            } else {
+                $mph_lastest = $ph_lastest->mahocsinh;
+                $mph_lastest = ((int) substr($mph_lastest, 2)) + 1;
+                $mph_lastest = "PH" . sprintf("%05d", $mph_lastest);
+            }
+
+            $phuhuynh->maphuhuynh = $mph_lastest;
+            $phuhuynh->save();
+
+            // Hiển thị thông báo thêm thành công
+            toastr()->success('Thêm mới thành công!', 'Thành công!');
+
+            return redirect()->route('phuhuynhManage.index');
+        } catch (Exception $e) {
+            echo 'Có lỗi phát sinh: ', $e->getMessage(), "\n";
+        }
+    }
+
+    public function edit_PH($id)
+    {
+        $page_title = "Chỉnh Sửa Thông tin - Phụ huynh - MS: ".$id;
+        $info = PhuHuynh::find($id);
+        return view('pages.hocsinh_phuhuynh.phuhuynh.edit', compact('page_title', 'info'));
+    }
+
+    public function update_PH(Request $request)
+    {
+        try {
+            $phuhuynh = PhuHuynh::find($request->maphuhuynh);
+
+            $matkhau = $phuhuynh->matkhau;
+            $phuhuynh->fill($request->toArray());
+            if ($request->matkhau == "" || $request->matkhau == null) {
+                $phuhuynh->matkhau = $matkhau;
+            }
+            $phuhuynh->save();
+            toastr()->success('Cập nhật thông tin thành công!', 'Thành công!');
+            return redirect()->route('hocsinhManage.index');
+        } catch (Exception $e) {
+            echo 'Có lỗi phát sinh: ', $e->getMessage(), "\n";
+        }
+    }
+
+    public function delete_PH($maphuhuynh)
+    {
+        try {
+            $phuhuynh = PhuHuynh::find($maphuhuynh);
+            $phuhuynh->delete();
+            toastr()->success('Xoá thành công!', 'Thành công!');
+            return redirect()->route('hocsinhManage.indexhocsinh');
+        } catch (QueryException $e) {
+            // Lỗi dữ liệu được sử dụng (cha-con)
+            if ($e->errorInfo[1] == 1451) {
+                toastr()->error('Xoá không thành công! Dữ liệu đã được sử dụng!', 'Lỗi!');
+                return redirect()->back();
+            }
+        } catch (Exception $e) {
+            echo 'Có lỗi phát sinh: ', $e->getMessage(), "\n";
+        }
     }
 }
